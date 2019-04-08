f36c2808db6dc4fe996bd8395dbccb04c7e7c5e8
coriagogo <pkcparis@gmail.com>
2019-04-08 16:50:59 -0400

    index js updated

--- js/index.js
+++ js/index.js
@@ -39,4 +39,94 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+
+
+// create nav items
+let services = document.getElementsByTagName('a')[0];
+services.innerText = siteContent['nav']['nav-item-1'];
+
+let product = document.getElementsByTagName('a')[1];
+product.innerText = siteContent['nav']['nav-item-2'];
+
+let vision = document.getElementsByTagName('a')[2];
+vision.innerText = siteContent['nav']['nav-item-3'];
+
+let features = document.getElementsByTagName('a')[3];
+features.innerText = siteContent['nav']['nav-item-4'];
+
+let about = document.getElementsByTagName('a')[4];
+about.innerText = siteContent['nav']['nav-item-5'];
+
+let contact = document.getElementsByTagName('a')[5];
+contact.innerText = siteContent['nav']['nav-item-6'];
+
+// cta
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaHeader = document.getElementsByTagName('h1')[0];
+ctaHeader.innerText = siteContent['cta']['h1'];
+
+let ctaBtn = document.getElementsByTagName('button')[0];
+ctaBtn.innerText = siteContent['cta']['button'];
+
+// main content headers
+
+let mainContentFeaturesH = document.getElementsByTagName('h4')[0];
+mainContentFeaturesH.innerText = siteContent['main-content']['features-h4'];
+
+let mainContentAboutH = document.getElementsByTagName('h4')[1];
+mainContentAboutH.innerText = siteContent['main-content']['about-h4'];
+
+let mainContentServicesH = document.getElementsByTagName('h4')[2];
+mainContentServicesH.innerText = siteContent['main-content']['services-h4'];
+
+let mainContentProductH = document.getElementsByTagName('h4')[3];
+mainContentProductH.innerText = siteContent['main-content']['product-h4'];
+
+let mainContentVisionH = document.getElementsByTagName('h4')[4];
+mainContentVisionH.innerText = siteContent['main-content']['vision-h4'];
+
+// middle image
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// main content
+
+let mainContentFeatures = document.getElementsByTagName('p')[0];
+mainContentFeatures.innerText = siteContent['main-content']['features-content'];
+
+let mainContentAbout = document.getElementsByTagName('p')[1];
+mainContentAbout.innerText = siteContent['main-content']['about-content'];
+
+let mainContentServices = document.getElementsByTagName('p')[2];
+mainContentServices.innerText = siteContent['main-content']['services-content'];
+
+let mainContentProduct = document.getElementsByTagName('p')[3];
+mainContentProduct.innerText = siteContent['main-content']['product-content'];
+
+let mainContentVision = document.getElementsByTagName('p')[4];
+mainContentVision.innerText = siteContent['main-content']['vision-content'];
+
+// contact
+
+let contactHeader = document.getElementsByTagName('h4')[5];
+contactHeader.innerText = siteContent['contact']['contact-h4'];
+
+let contactAddress = document.getElementsByTagName('p')[5];
+contactAddress.innerText = siteContent['contact']['address'];
+
+let contactPhone = document.getElementsByTagName('p')[6];
+contactPhone.innerText = siteContent['contact']['phone'];
+
+let contactEmail = document.getElementsByTagName('p')[7];
+contactEmail.innerText = siteContent['contact']['email'];
+
+// footer
+
+let footerCopy = document.getElementsByTagName('p')[8];
+footerCopy.innerText = siteContent['footer']['copyright'];
+
